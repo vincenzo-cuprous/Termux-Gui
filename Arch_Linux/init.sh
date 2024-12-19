c04a68f7577abc633457f4bae5720da47e1db372
vincenzo-cuprous <159692904+vincenzo-cuprous@users.noreply.github.com>
2024-12-19 14:15:51 +0500

    Update init.sh

--- Arch_Linux/init.sh
+++ Arch_Linux/init.sh
@@ -5,4 +5,6 @@ pkg update ;
 pkg update ;
 pkg update ;
 pkg upgrade -y ;
-pkg install fish proot proot-distro root-repo x11-repo -y ;
+#pkg install fish proot proot-distro root-repo x11-repo -y ;
+pkg install fish ;
+pkg install proot ;
